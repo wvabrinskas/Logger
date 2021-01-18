ae9a8a40f59b530e42cc0c63b2e7567a4ecb8d40
William Vabrinskas <wvabrinskas@gmail.com>
2021-01-17 21:15:05 -0500

    added logger

---
+++ Logger/Tests/LoggerTests/LoggerTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import Logger
+
+final class LoggerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
